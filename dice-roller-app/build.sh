de7ae84108dfcac5d8be72337f8627d44dd3c66a
Remco Beckers <remco.beckers@suse.com>
2025-04-18 17:38:43 +0200

    Rename docker image tag

--- dice-roller-app/build.sh
+++ dice-roller-app/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t ghcr.io/rb3ckers/dice-roller-app .
-docker push ghcr.io/rb3ckers/dice-roller-app
+docker build -t ghcr.io/rb3ckers/dice-roller-app:python .
+docker push ghcr.io/rb3ckers/dice-roller-app:python
